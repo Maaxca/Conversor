1eccb5f3693a0e4d71d205cfd533284d9788258f
Maaxca <marioacedo@iesflorenciopintado.es>
2022-02-14 10:31:22 +0100

    Acabado6

--- app/src/androidTest/java/com/algo/conversor/MainActivityEspresso.java
+++ app/src/androidTest/java/com/algo/conversor/MainActivityEspresso.java
@@ -17,9 +17,16 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+/**
+ * The type Main activity espresso.
+ */
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class MainActivityEspresso {
+    /**
+     * The Main activity activity test rule.
+     * @author Maaxca
+     */
     @Rule
     public ActivityScenarioRule<MainActivity> mainActivityActivityTestRule=new ActivityScenarioRule<>(MainActivity.class);
 
@@ -33,6 +40,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.MillasKms)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("1.60934")));
     }
+
+    /**
+     * Prueba millas kms 2.
+     */
     @Test
     public void pruebaMillasKms2(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -40,6 +51,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.MillasKms)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("160.934")));
     }
+
+    /**
+     * Prueba millas kms 3.
+     */
     @Test
     public void pruebaMillasKms3(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -47,6 +62,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.MillasKms)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("32.1868")));
     }
+
+    /**
+     * Prueba millas kms 4.
+     */
     @Test
     public void pruebaMillasKms4(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -54,6 +73,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.MillasKms)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("-160.934")));
     }
+
+    /**
+     * Prueba millas kms 5.
+     */
     @Test
     public void pruebaMillasKms5(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -61,6 +84,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.MillasKms)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("12.87472")));
     }
+
+    /**
+     * Prueba millas kms 6.
+     */
     @Test
     public void pruebaMillasKms6(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -68,6 +95,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.MillasKms)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("Error")));
     }
+
+    /**
+     * Prueba kms millas 1.
+     */
     @Test
     public void pruebaKmsMillas1(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -75,6 +106,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.KmsMillas)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("1.0")));
     }
+
+    /**
+     * Prueba kms millas 2.
+     */
     @Test
     public void pruebaKmsMillas2(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -82,6 +117,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.KmsMillas)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("100.0")));
     }
+
+    /**
+     * Prueba kms millas 3.
+     */
     @Test
     public void pruebaKmsMillas3(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -89,6 +128,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.KmsMillas)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("20.0")));
     }
+
+    /**
+     * Prueba kms millas 4.
+     */
     @Test
     public void pruebaKmsMillas4(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -96,6 +139,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.KmsMillas)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("-100.0")));
     }
+
+    /**
+     * Prueba kms millas 5.
+     */
     @Test
     public void pruebaKmsMillas5(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -103,6 +150,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.KmsMillas)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("8.0")));
     }
+
+    /**
+     * Prueba kms millas 6.
+     */
     @Test
     public void pruebaKmsMillas6(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -110,6 +161,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.KmsMillas)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("Error")));
     }
+
+    /**
+     * Prueba c farenheit 1.
+     */
     @Test
     public void pruebaCFarenheit1(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -117,6 +172,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.CFarenheit)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("32.0")));
     }
+
+    /**
+     * Prueba c farenheit 2.
+     */
     @Test
     public void pruebaCFarenheit2(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -124,6 +183,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.CFarenheit)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("212.0")));
     }
+
+    /**
+     * Prueba c farenheit 3.
+     */
     @Test
     public void pruebaCFarenheit3(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -131,6 +194,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.CFarenheit)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("68.0")));
     }
+
+    /**
+     * Prueba c farenheit 4.
+     */
     @Test
     public void pruebaCFarenheit4(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -138,6 +205,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.CFarenheit)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("-148.0")));
     }
+
+    /**
+     * Prueba c farenheit 5.
+     */
     @Test
     public void pruebaCFarenheit5(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -145,6 +216,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.CFarenheit)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("46.4")));
     }
+
+    /**
+     * Prueba c farenheit 6.
+     */
     @Test
     public void pruebaCFarenheit6(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -152,6 +227,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.CFarenheit)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("Error")));
     }
+
+    /**
+     * Prueba farenheit c 1.
+     */
     @Test
     public void pruebaFarenheitC1(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -159,6 +238,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.FarenheitC)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("0.0")));
     }
+
+    /**
+     * Prueba farenheit c 2.
+     */
     @Test
     public void pruebaFarenheitC2(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -166,6 +249,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.FarenheitC)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("100.0")));
     }
+
+    /**
+     * Prueba farenheit c 3.
+     */
     @Test
     public void pruebaFarenheitC3(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -173,6 +260,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.FarenheitC)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("20.0")));
     }
+
+    /**
+     * Prueba farenheit c 4.
+     */
     @Test
     public void pruebaFarenheitC4(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -180,6 +271,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.FarenheitC)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("-100.0")));
     }
+
+    /**
+     * Prueba farenheit c 5.
+     */
     @Test
     public void pruebaFarenheitC5(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
@@ -187,6 +282,10 @@ public class MainActivityEspresso {
         Espresso.onView(withId(R.id.FarenheitC)).perform(click());
         Espresso.onView(withId(R.id.salida)).check(matches(withText("8.0")));
     }
+
+    /**
+     * Prueba farenheit c 6.
+     */
     @Test
     public void pruebaFarenheitC6(){
         Espresso.onView(withId(R.id.entrada)).perform(clearText());
